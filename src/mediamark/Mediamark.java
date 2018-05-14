552236ae3d1a963708824eeec5f3121d4023d150
Marcfernic <1642969@alu.murciaeduca.es>
2018-05-14 09:43:44 +0200

    mediamark 1

---
+++ src/mediamark/Mediamark.java
@@ -0,0 +1,37 @@
+
+package mediamark;
+import java.sql.*;
+
+public class Mediamark {
+
+  
+    public static void main(String[] args) {
+        cargarDatos();
+    }
+
+    private static void cargarDatos() {
+        try{
+            //1.Cargar el driver
+            Class.forName("com.mysql.jdbc.Driver");
+            //2. Creo la conexion contra la BD 
+            Connection c=
+                    DriverManager.getConnection("jdbc:mysql://localhost/mediamark","alumno","alumno");
+            // 3. Creo el Statement
+            Statement s=c.createStatement();
+            //4.Trabajo con las consultas
+            ResultSet rs=s.executeQuery("Select * From Productos");
+            while(rs.next()==true){
+                System.out.println(rs.getInt("Id")+" "+rs.getString("Nombre")+" "+rs.getDouble("Precio"));
+            }
+            rs.close();
+            s.close();
+            c.close();
+        } catch(Exception e){
+            System.out.println("Error en la base de datos"); 
+            e.printStackTrace();
+            //Muestra informacion del error
+        }
+  
+    }
+    
+}
